6db3460a7323ac95be88bcdf529b7f7dcedcbdcc
anuj <anuj00000010@gmail.com>
2023-09-17 19:00:16 +0530

    made some changes in the ui

--- src/components/Choice.jsx
+++ src/components/Choice.jsx
@@ -22,13 +22,14 @@ export default function Choice() {
       <Typography variant="p" sx={{ fontSize: "2rem", color: "#1976d2" }}>
         Are you a ?
       </Typography>
-      <Box sx={{ display: "flex", gap: "1rem" }}>
+      <Box sx={{ display: "flex", gap:{xs:"0.5rem",sm:"1rem"},padding:"0.5rem",flexWrap:"wrap" }}>
         {experience.map((exp, index) => {
           return (
             <Button
               variant={selectedSection === exp ? "contained" : "outlined"}
               key={index}
               onClick={() => setSelectedSection(exp)}
+              sx={{fontSize:{xs:"0.7rem",sm:"1rem"}}}
             >
               {exp}
             </Button>
